ebabba583d7abbca3b33de6f3d19fe78db572e4e
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2020-09-01 14:23:50 -0400

    Parse and load packing methods

--- cfr_tool/explosives.py
+++ cfr_tool/explosives.py
@@ -1,3 +1,5 @@
+import re
+
 class Explosives:
     def __init__(self, db, soup):
         self.db = db
@@ -31,4 +33,56 @@ class Explosives:
         ''', entries)
         print("loaded explosives table")
 
-    #def parse_packing_methods(soup):
+    def parse_load_packing_methods(self):
+        self.db.executescript("DROP TABLE IF EXISTS packing_methods;")
+        self.db.executescript('''
+            CREATE TABLE packing_methods (
+                packaging_instruction text,
+                inner_packagings_type text,
+                intermediate_packagings_type text,
+                outer_packagings_type text,
+                requirements_exceptions text,
+                inner_packagings_material text,
+                intermediate_packagings_material text,
+                outer_packagings_material text,
+                FOREIGN KEY (packaging_instruction)
+                    REFERENCES explosives_table (packaging_instruction)
+            );
+        ''')
+        packing_rows = self.soup.find_table("Table of Packing Methods").find_all('row')
+        symbol = True
+        full_data = []
+        for row in packing_rows:
+            ents = row.find_all('ent')
+            if symbol:
+                #make sure there are three digits at the beginning
+                three_digits = re.compile("\d\d\d")
+                assert three_digits.match(ents[0].text)
+                data = [ent.text for ent in ents]
+                while len(data) < 4:
+                    data.append(None)
+            else:
+                for ent in ents:
+                    #skip the rows that just say "PARTICULAR PACKING REQUIREMENTS OR EXCEPTIONS:"
+                    if ent.text == "PARTICULAR PACKING REQUIREMENTS OR EXCEPTIONS:" and \
+                        len(ents) == 1:
+                        continue
+                    data.append(ent.text)
+                while len(data) < 8:
+                    data.append(None)
+                full_data.append(tuple(data))
+            symbol = not symbol
+        self.db.executemany('''
+            INSERT INTO packing_methods (
+                packaging_instruction,
+                inner_packagings_type,
+                intermediate_packagings_type,
+                outer_packagings_type,
+                requirements_exceptions,
+                inner_packagings_material,
+                intermediate_packagings_material,
+                outer_packagings_material
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', full_data)
+
+
