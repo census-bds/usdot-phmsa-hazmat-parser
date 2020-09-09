a222d9c6be0f1f22a52c3ea51ecadd6f76c60748
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2020-09-09 14:14:26 -0400

    modify parse packaging codes regex and write parse_packaging_names to get material names

--- cfr_tool/clean_text.py
+++ cfr_tool/clean_text.py
@@ -8,7 +8,12 @@ def clean_new_lines(ent):
     return ent.text.strip('\n').replace('\n', ' ')
 
 def parse_packaging_codes(text):
-    pattern = re.compile('\d\w\d?')
+    #TO DO: make look behind a fixed width pattern
+    pattern = re.compile('(?<=(..\()|(\sor\s))(\d[A-Z]\d?(/\d)*)*(?=(\)|(\sor\s)))')
+    return pattern.findall(text)
+
+def parse_packaging_names(text):
+    pattern = re.compile('[a-zA-Z,\s-]+(?=((\s)|(\W))*\((\w|\s|/)*\))')
     return pattern.findall(text)
 
     
