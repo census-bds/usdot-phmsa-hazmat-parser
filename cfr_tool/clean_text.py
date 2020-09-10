e359ad61677476ac0bda3a86e2d02cbdf009efa0
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2020-09-10 17:23:26 -0400

    Wrote a single function which combines parsing of packaging names and their corresponding codes into one

--- cfr_tool/clean_text.py
+++ cfr_tool/clean_text.py
@@ -9,7 +9,7 @@ def clean_new_lines(ent):
 
 def parse_packaging_codes(text):
     #TO DO: deal with longer lists like those in PI 117: metal (11A), (11B), (11N), (21A), (21B), (21N), (31A), (31B), (31N)
-    pattern = re.compile('(?<=(\()|(\sor\s))(\d+[A-Z]\d?(/\d)*)(?=(\)|(\sor\s)))')
+    pattern = re.compile('(?<=(\()|(\sor\s))(\d+[A-Z]+\d?(/\d)*)(?=(\)|(\sor\s)))')
     matches = pattern.findall(text)
     codes = []
     first_entry = None
@@ -26,7 +26,25 @@ def parse_packaging_codes(text):
 
 def parse_packaging_names(text):
     #TO DO: deal with typos where there is no space after a period
-    pattern = re.compile('((?<!\d)(?<=\s|^|.\s|.)[^\d\(\).]+(?=[\s|-]\(\d+[A-Z]))')
+    pattern = re.compile('(?<!\d)(?<=\s|^|.\s|.)[^\d\(\).]+\w+(?=[\s|-]\(\d+[A-Z])')
     return [t.strip() for t in pattern.findall(text)]
 
-    
+def parse_names_codes(text):
+    pattern = re.compile(
+        '((?<!\d)(?<=\s|^|.\s|.)[^\d\(\).]+\w+(?=[\s|-]\(\d+[A-Z]))|(?<=\(|\sor\s)(\d+[A-Z]+\d?(/\d)*)(?=\)|\sor\s)')
+    results = pattern.findall(text)
+    last_name = None
+    output = {}
+    for name, code, slashes in results:
+        if name:
+            output[name.strip()] = []
+            last_name = name.strip()
+        if slashes:
+            base = code[0:code.find("/")]
+            output[last_name].append(base)
+            for trailing_number in code[code.find("/"):].split("/"):
+                if trailing_number:
+                    output[last_name].append(base[:-1] + trailing_number)
+        if code and slashes == '':
+            output[last_name].append(code)
+    return output
