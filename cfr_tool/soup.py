2a1d032f2d162fe19cbe01a426f668a7b6b2ebc9
mosca303 <christian.l.moscardi@census.gov>
2020-09-29 13:45:56 -0400

    general functions to pull out paragraph sub-structure

--- cfr_tool/soup.py
+++ cfr_tool/soup.py
@@ -1,9 +1,12 @@
+from collections import defaultdict
 import logging
 import os
 
+import regex as re
 import requests
 import bs4
 import xml.etree.ElementTree as ET
+import networkx as nx
 
 class Soup:
     CACHE_DIRECTORY = "cfr_cache"
@@ -45,3 +48,49 @@ class Soup:
             'sectno', text="§ {}.{}".format(str(part), str(subpart)))
         return subpart_tag.parent
 
+    def get_subpart_paragraphs(self, part, subpart):
+        """
+        returns: a networkx graph object of the paragraphs of this subpart
+        """
+        subpart = self.get_subpart_text(part, subpart)
+        paragraphs = subpart.find_all("p")
+        indexed = list(self.gen_paragraph_tree(paragraphs))
+        ret_tree = nx.Graph()
+        for ix, paragraph in indexed:
+            canonical = ".".join(i for i in ix if i)
+            parent = ".".join(canonical.split(".")[:-1])
+            ret_tree.add_node(canonical, paragraph=paragraph)
+            if parent:
+                ret_tree.add_edge(parent, canonical)
+
+        return ret_tree 
+
+
+    @staticmethod
+    def gen_paragraph_tree(paragraphs):
+        # this reflects the nested paragraph structure
+        # e.g. you could look at
+        # part 178 subpart 500 paragraph a subparagraph 1
+        # sub-sub paragraph i sub-sub-sub paragraph A
+        # i.e. 178.500 (a)(1)(i)(A)
+        letter_pattern = re.compile(r'\(([a-z])\)')
+        number_pattern = re.compile(r'\(([0-9]+)\)')
+        numeral_pattern = re.compile(r'\(([ivx]+)\)') # this is horrible
+        uppercase_letter_pattern = re.compile(r'\(([A-Z])\)')
+
+        patterns = (letter_pattern, number_pattern, numeral_pattern, uppercase_letter_pattern)
+        # letter, number, numeral, uppercase
+        indices = [None, None, None, None]
+        def _reset_indices_after(ix):
+            for i in range(ix + 1, len(indices)):
+                indices[i] = None
+
+        for paragraph in paragraphs:
+            beginning = paragraph.text.strip()[:6]
+            for ix, pattern in enumerate(patterns):
+                match = pattern.findall(beginning)
+                if match:
+                    assert len(match) == 1
+                    indices[ix] = match[0]
+                    _reset_indices_after(ix)
+                    yield tuple(indices), paragraph
