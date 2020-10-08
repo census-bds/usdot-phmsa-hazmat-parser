ecec993418300ddd1a21b2599ad26403503b338e
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2020-10-08 16:43:24 -0400

    Wrote a unna_lookup function that takes a code, looks through the loaded hazmat table in db, returns the allowable packaging code.

--- cfr_tool/instructions.py
+++ cfr_tool/instructions.py
@@ -1,10 +1,19 @@
-from .packaging_codes import PackagingCodes
-from .soup import Soup
+from packaging_codes import PackagingCodes
+from soup import Soup
 
 class Instructions(PackagingCodes):
-    PART = 173
     START = 201
     END = 202
     def __init__(self, db, soup):
         PackagingCodes.__init__(self, db, soup)
-        self.code_descriptions = self.get_codes_descriptions(self.START, self.END)
+        self.PART = 173
+
+    def unna_lookup(self, code, bulk):
+        table = "bulk_packaging" if bulk else "non_bulk_packaging"
+        requirement = self.db.execute('''
+            SELECT requirement FROM hazmat_table
+            JOIN {}
+            ON hazmat_table.hazmat_id = {}.hazmat_id
+            WHERE hazmat_table.id_num = '{}';
+        '''.format(table, table, code))
+        return self.get_codes_descriptions(int(requirement.fetchone()[0]), 'b')
