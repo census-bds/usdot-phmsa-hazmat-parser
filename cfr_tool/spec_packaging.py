1729da00149845b1a2825297ad789f814a61d730
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2020-12-15 17:14:15 -0500

    Parses tank car codes and descriptions from subpart headers of part 179

--- cfr_tool/spec_packaging.py
+++ cfr_tool/spec_packaging.py
@@ -1,21 +1,43 @@
 from . import soup
+import regex as re
+#need to rename repo without hyphen so this won't be necessary
+import importlib
+pattern =  importlib.import_module("cfr_tool.phmsa-package-regexps.patterns")
 
 class SpecPackaging:
     def __init__(self, db):
         self.s = soup.Soup(3)
-        self.part_178 = self.s.parsed_soup.find_all('ear', text='Pt. 178')[0].parent
+    
+    def get_subparts(self, part):
+        part = self.s.parsed_soup.find_all('ear', text='Pt. {}'.format(part))[0].parent
+        subparts = part.find_all('hd')
+        return [subpart for subpart in subparts \
+            if "Subpart" in subpart.text and "Specifications" in subpart.text]
 
     def get_sects_subjects(self):
-        #sectnos_178 = self.part_178.find_all('sectno')
-        subparts = self.part_178.find_all('hd')
-        subparts = [subpart for subpart in subparts \
-            if "Subpart" in subpart.text and "Specifications" in subpart.text]
+        subparts = self.get_subparts(178)
+        output = []
         for subpart in subparts:
             sectnos = subpart.parent.find_all('sectno')
-            for sectno in secnos:
+            for sectno in sectnos:
                 subject = sectno.find_next()
-                assert subject.name == 'subject'
-                
-        return {sect.text: sect.find_next().text for sect in sectnos_178 \
-            if sect.find_next().text[:13] == 'Specification'} 
+                if subject.name != 'subject':
+                    continue
+                spec_pattern = re.compile(pattern.SPEC_PACKAGING)
+                match = spec_pattern.findall(subject.text)
+                if len(match) == 0:
+                    continue
+                assert len(match) == 1
+                output.append((sectno.text, match[0][0], match[0][1]))
+        return output
     
+    def get_tank_cars(self):
+        subparts = self.get_subparts(179)
+        for subpart in subparts:
+            tank_car_pattern = re.compile(pattern.TANK_CAR_CODE)
+            codes = tank_car_pattern.findall(subpart.text)
+            description_pattern = re.compile(pattern.TANK_CAR_DESCRIPTION)
+            description = description_pattern.findall(subpart.text)
+            print(codes)
+            print(description)
+            print()
