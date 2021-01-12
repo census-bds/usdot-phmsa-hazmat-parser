689bda948587797ec52d545239c1fc10c8824cbc
Krista Cheung Chan (CENSUS/ERD CTR) <krista.c.chan@census.gov>
2021-01-12 11:42:07 -0500

    performance packaging standards populate on click

--- cfr_tool/code_lookup.py
+++ cfr_tool/code_lookup.py
@@ -1,9 +1,32 @@
 import json
 import flask
 
+from . import db
+from . import packaging_codes as pc
+from . import soup
+from . import clean_text as ct
+
 def code_lookup():
     code = flask.request.args.get("code")
     if code:
-        return json.dumps({"the code is": code})
+        cur = db.get_db().cursor()
+        cur.execute('''
+            SELECT * FROM packaging_standards WHERE full_code='{}'
+        '''.format(code))
+        rows = cur.fetchall()
+        #For now, grabs the first result
+        subpart = rows[0]['subpart']
+        p = pc.PackagingCodes(cur, soup.Soup(3))
+        p.part = 178
+        spans_paragraphs = p.get_spans_paragraphs(subpart)
+        html_list = ct.build_packaging_text(spans_paragraphs)
+        html_text = ''
+        for p in html_list:
+            html_text += '<p>'
+            html_text += p
+            html_text += '</p>'
+        return json.dumps({"status": "success",
+                           "subpart": subpart,
+                           "html": html_text})
     else:
-        return json.dumps({"a test of": "an api endpoint"})
+        return json.dumps({"status": "code not found"})
